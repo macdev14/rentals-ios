b2311a4fa80e9c744f3830d7a85a14b3df1dedd9
Lauro <lauropimentel12@gmail.com>
2024-03-28 15:34:58 -0300

    Initial Commit

---
+++ rentals/rentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rentalsApp.swift
+//  rentals
+//
+//  Created by Lauro Pimentel on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct rentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
